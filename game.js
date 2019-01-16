35c023247f9374a50c20f9abe90db0fcf28d4693
Wong, Pak Yin <pakyin.wong@link.cuhk.edu.hk>
2019-01-16 22:16:02 +0800

    paddle generalized

--- game.js
+++ game.js
@@ -3,94 +3,36 @@ $(document).ready(function() {
     Crafty.background('rgb(127,127,127)');
 
     Crafty.scene("game", function(){
+        Crafty.c("Paddle", {
+            paddle: function(startposx, startposy) {
+                this.color('rgb(255,0,0)');
+                this.attr({ x: startposx, y: startposy, w: 100, h: 10, paddlespeed: 1})
+                this.bind('UpdateFrame', function () {
+                    if (this.y <= 0){
+                        this.x = Crafty.math.randomInt(50, 449);
+                        this.y = 390;
+                        Crafty("Points").each(function () {
+                            this.text("Passed " + ++this.points + " Paddles") });
+                    }
+                    this.y -= this.paddlespeed;
+                })
+                this.onHit('Man', function(){
+                            var paddley = this.y;
+                            var paddlex = this.x;
+                            Crafty("Man").each(function () {
+                                this.y = paddley - 10;
+                            });
+                        });
+                return this;
+            }
+        });
+
         //Paddles
-        var Paddle1 = Crafty.e("Paddle, 2D, Collision, DOM, Color")
-            .color('rgb(255,0,0)')
-            .attr({ x: 250, y: 290, w: 100, h: 10, paddlespeed: 1})
-            .bind('UpdateFrame', function () {
-                if (this.y <= 0){
-                    this.x = Crafty.math.randomInt(50, 449);
-                    this.y = 390;
-                    Crafty("Points").each(function () {
-                        this.text("Passed " + ++this.points + " Paddles") });
-                }
-                this.y -= this.paddlespeed;
-            })
-            .onHit('Man', function(){
-                        var paddley = this.y;
-                        var paddlex = this.x;
-                        Crafty("Man").each(function () {
-                            this.y = paddley - 10;
-                        });
-                    }
-                );
-        
-        var Paddle2 = Crafty.e("Paddle, 2D, Collision, DOM, Color")
-            .color('rgb(255,0,0)')
-            .attr({ x: Crafty.math.randomInt(50,450), y: 390, w: 100, h: 10, paddlespeed: 1})
-            .bind('UpdateFrame', function () {
-                if (this.y <= 0){
-                    this.x = Crafty.math.randomInt(50, 449);
-                    this.y = 390;
-                    Crafty("Points").each(function () {
-                        this.text("Passed " + ++this.points + " Paddles") });
-                }
-                this.y -= this.paddlespeed;
-            })
-            .onHit('Man', function(){
-                        var paddley = this.y;
-                        var paddlex = this.x;
-                        Crafty("Man").each(function () {
-
-                            this.y = paddley - 10;
-                        });
-                    }
-                );
-
-        var Paddle3 = Crafty.e("Paddle, 2D, Collision, DOM, Color")
-            .color('rgb(255,0,0)')
-            .attr({ x: Crafty.math.randomInt(50,450), y: 490, w: 100, h: 10, paddlespeed: 1})
-            .bind('UpdateFrame', function () {
-                if (this.y <= 0){
-                    this.x = Crafty.math.randomInt(50, 449);
-                    this.y = 390;
-                    Crafty("Points").each(function () {
-                        this.text("Passed " + ++this.points + " Paddles") });
-                }
-                this.y -= this.paddlespeed;
-            })
-            .onHit('Man', function(){
-                        var paddley = this.y;
-                        var paddlex = this.x;
-                        Crafty("Man").each(function () {
-
-                            this.y = paddley - 10;
-                        });
-                    }
-                );
-
-        var Paddle4 = Crafty.e("Paddle, 2D, Collision, DOM, Color")
-            .color('rgb(255,0,0)')
-            .attr({ x: Crafty.math.randomInt(50,450), y: 590, w: 100, h: 10, paddlespeed: 1})
-            .bind('UpdateFrame', function () {
-                if (this.y <= 0){
-                    this.x = Crafty.math.randomInt(50, 449);
-                    this.y = 390;
-                    Crafty("Points").each(function () {
-                        this.text("Passed " + ++this.points + " Paddles") });
-                }
-                this.y -= this.paddlespeed;
-            })
-            .onHit('Man', function(){
-                        var paddley = this.y;
-                        var paddlex = this.x;
-                        Crafty("Man").each(function () {
-
-                            this.y = paddley - 10;
-                        });
-                    }
-                );
-        
+        var Paddle1 = Crafty.e("Paddle, 2D, Collision, DOM, Color").paddle(250, 290);    
+        var Paddle2 = Crafty.e("Paddle, 2D, Collision, DOM, Color").paddle(Crafty.math.randomInt(50,450), 390);    
+        var Paddle3 = Crafty.e("Paddle, 2D, Collision, DOM, Color").paddle(Crafty.math.randomInt(50,450), 490);    
+        var Paddle4 = Crafty.e("Paddle, 2D, Collision, DOM, Color").paddle(Crafty.math.randomInt(50,450), 590);    
+    
         //Man
         Crafty.e("Man, 2D, DOM, Color, Gravity, Multiway")
             .attr({ x: 295, y: 90, w: 10, h: 10 })
